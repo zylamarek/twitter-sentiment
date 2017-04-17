555b7d58912a22d365e87ac964203710133f0e68
zylamarek <zylamarek@gmail.com>
2017-04-17 17:30:01 +0200

    log stderr to file

--- train.py
+++ train.py
@@ -140,6 +140,23 @@ fh.setFormatter(formatter)
 logger.addHandler(ch)
 logger.addHandler(fh)
 
+
+class StreamToLogger(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    From: https://www.electricmonk.nl/log/2011/08/14/redirect-stdout-and-stderr-to-a-logger-in-python/
+    """
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+sys.stderr = StreamToLogger(logger, logging.ERROR)
+
 # Log parameters
 logger.info('Settings')
 col_wid = max([len(name) for name, _ in args._get_kwargs()] + [24]) + 1
